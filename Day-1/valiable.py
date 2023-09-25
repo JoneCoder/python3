140201892c74d0b7e51def41e00243bd1730a1b8
JoneCoder <mohummadshorifulislam@gmail.com>
2023-09-26 01:14:54 +0600

    added day-2 lesson float data type

---
+++ Day-1/valiable.py
@@ -0,0 +1,18 @@
+name = "Hassan"  # String data type
+age = 18  # Int data type
+phone = "+880122434535"  # String data type
+
+# Operator
+
+# + addition
+# - subtraction
+# * multiplication
+# / division
+
+# Conditional  Operator
+
+# ==, !=
+
+# Logical  Operator
+
+# and
---
+++ Day-2/data_type.py
@@ -0,0 +1,33 @@
+
+
+number1 = 5
+number2 = 12
+result = number1 + number2
+
+userNumber1 = int(input('Enter a integer number:'))
+userNumber2 = int(input('Enter an another integer number:'))
+addingResult = userNumber1 + userNumber2
+
+typOfData = type(addingResult)
+print(addingResult)
+print(typOfData)
+
+countryName = "Bangladesh"
+typeOfName = type(countryName)
+print(typeOfName)
+
+
+gpa = 4.5
+typeOfGpa = type(gpa)
+print(typeOfGpa)
+
+userValue1 = input('Enter a number:')
+userValue2 = input('Enter an another number:')
+
+userValue1 = float(userValue1)
+userValue2 = float(userValue2)
+
+addingResult = userNumber1 + userNumber2
+
+print(addingResult)
+
