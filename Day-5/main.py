2e0d29704a501d17d79f95c8c4a8996fb9f98eb8
JoneCoder <mohummadshorifulislam@gmail.com>
2023-10-02 23:22:24 +0600

    add day 5 lesson, for loop

---
+++ Day-5/main.py
@@ -0,0 +1,25 @@
+# print("Hello Turtle")
+import turtle
+
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.exitonclick()
+
+# turtle.forward(100)
+# turtle.left(90 + 30)
+# turtle.forward(100)
+# turtle.left(90 + 30)
+# turtle.forward(100)
+# turtle.exitonclick()
+
+# Loop
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in numbers:
+    print("2 x ", i, " = ", 2 * i, "|", "4 x ", i, " = ", 4 * i)
+
