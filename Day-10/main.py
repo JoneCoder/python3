5ad11704db870ec24897a5b632cdf094bf3da844
JoneCoder <mohummadshorifulislam@gmail.com>
2023-10-15 00:13:48 +0600

    add lession day-10

---
+++ Day-10/main.py
@@ -0,0 +1,12 @@
+# Scop
+name = "Bangledesh"
+numbers = [1, 3, 4, 6, 7, 8, 29, 3, 24]
+
+def myFunction():
+    age = 23
+    mark = 86
+    result = age + mark
+    return str(age) + " " + name
+
+
+print(myFunction())
