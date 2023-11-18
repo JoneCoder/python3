417e7c2de49aa50572685ee12621ba45abcd9c9a
JoneCoder <mohummadshorifulislam@gmail.com>
2023-11-18 23:19:50 +0600

    add day 19 lession

---
+++ Day-19/main.py
@@ -0,0 +1,28 @@
+# import math
+#
+# pi = math.pi
+# power = math.pow(2, 3)
+# sqr = math.sqrt(25)
+# floor = math.floor(5.2)
+# ceil = math.ceil(5.2)
+#
+# print(ceil)
+
+# import datetime
+# today = datetime.date.today()
+# todayTime = datetime.datetime.today()
+
+# while True:
+#     today = datetime.datetime.today()
+#     print(today)
+
+# from datetime import datetime
+#
+# todayTime = datetime.today()
+# print(todayTime)
+
+import webbrowser as web
+url = "https://www.linkedin.com/in/jonecoder"
+web.open(url)
+
+
